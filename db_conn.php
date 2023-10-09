7abbeb7d7a70a03c1a485a60590c63d4d996ca41
victormusembi11 <victor.musembi11@gmail.com>
2023-10-09 19:11:08 +0300

    database connection

---
+++ db_conn.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "qw3rty1234";
+$database = "MyDB";
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to database";
